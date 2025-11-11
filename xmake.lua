2e85910219801b22360f39eae439913af9dae078
Norbert <125549982+pewpewnor@users.noreply.github.com>
2025-11-12 04:04:55 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,4 @@
 add_rules("mode.debug", "mode.release")
-
-set_languages("c++23")
-set_warnings("allextra", "pedantic", "error")
-
 if is_mode("debug") then
     set_policy("build.sanitizer.address", true)
     set_policy("build.sanitizer.leak", true)
@@ -10,6 +6,9 @@ if is_mode("debug") then
 elseif is_mode("tsan") then
     set_policy("build.sanitizer.thread", true)
 end
+
+set_languages("c++23")
+set_warnings("allextra", "pedantic", "error")
 
 target("project-name")
     set_kind("binary")
