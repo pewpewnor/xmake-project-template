8791dc0325091054944d6db995f802fc6cb9fa0c
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2025-11-27 02:45:00 +0800

    Enhance xmake.lua with sanitizer configurations
    Added support for debug and thread sanitizer modes.

--- xmake.lua
+++ xmake.lua
@@ -1,14 +1,22 @@
+set_languages("c++23")
+set_warnings("allextra", "pedantic", "error")
+set_toolchains("clang")
+set_runtimes("c++_static")
+set_kind("static")
+
 add_rules("mode.debug", "mode.release")
 if is_mode("debug") then
     set_policy("build.sanitizer.address", true)
     set_policy("build.sanitizer.leak", true)
     set_policy("build.sanitizer.undefined", true)
-elseif is_mode("tsan") then
+    add_defines("DEBUG")
+elseif is_mode("debug_tsan") then
+    set_mode("debug")
     set_policy("build.sanitizer.thread", true)
+    add_defines("DEBUG")
 end
 
-set_languages("c++23")
-set_warnings("allextra", "pedantic", "error")
+add_requireconfs("*", { configs = { shared = not is_mode("release") } })
 
 target("project-name")
     set_kind("binary")
